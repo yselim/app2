669a5aed63b462ede95291915f5cd94fec4ba13f
yavuz dogan <y.selimdogan@gmail.com>
2024-12-19 20:11:29 +0300

    initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,63 +1,101 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {
+  Image,
+  StyleSheet,
+  Platform,
+  View,
+  Text,
+  SafeAreaView,
+  Pressable,
+} from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { useState } from "react";
 
 export default function HomeScreen() {
+  const BOX_AMOUNT = 70;
+  const HIGLIGHT_BOX_AMOUNT = 3;
+
+  const [boxes, setBoxes] = useState<number[]>(
+    Array.from({ length: BOX_AMOUNT }, (_, i) => 0) // [0,0,0,0,1,0,0,00,]
+  );
+
+  const [randomlySelectedIndexes, setRandomlySelectedIndexes] = useState<
+    number[]
+  >([]);
+
+  const renderABox = (isHighligting: number, index: number) => {
+    return (
+      <Pressable
+        onPress={() => {
+          const newBoxes = [...boxes];
+          newBoxes[index] = 1;
+          setBoxes(newBoxes);
+        }}
+        key={index}
+        style={{
+          width: 50,
+          height: 50,
+          backgroundColor: isHighligting ? "red" : "powderblue",
+          borderWidth: 1,
+        }}
+      ></Pressable>
+    );
+  };
+
+  const startGamme = async () => {
+    const intialBoxes = [...boxes];
+    const randomlySelectedBoxIndexes: number[] = [];
+    for (let i = 0; i < HIGLIGHT_BOX_AMOUNT; i++) {
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          const newBoxes = [...boxes];
+          const boxIndexToBeHighleted = Math.floor(Math.random() * BOX_AMOUNT);
+          randomlySelectedBoxIndexes.push(boxIndexToBeHighleted);
+          newBoxes[boxIndexToBeHighleted] = 1;
+          setBoxes(newBoxes);
+          console.log("newBoxes: ", newBoxes);
+          resolve(undefined);
+        }, 1000);
+      });
+    }
+
+    setBoxes(intialBoxes);
+    setRandomlySelectedIndexes(randomlySelectedBoxIndexes);
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView>
+      <View
+        style={{
+          padding: 16,
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        {boxes.map((boxNumber, index) => {
+          return renderABox(boxNumber, index);
+        })}
+
+        <Pressable
+          style={{ padding: 16, backgroundColor: "lightblue", marginTop: 16 }}
+          onPress={startGamme}
+        >
+          <Text>START GAME</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -69,6 +107,6 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
